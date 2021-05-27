f2de56bb6cd582adbf7af571d35c7de7c51ba349
ottonsam <ottonsam@gmail.com>
2021-05-27 04:30:33 -0300

    create transationstable component

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Website Development</td>
+            <td className="deposit">R$12.000</td>
+            <td>Development team</td>
+            <td>20/02/2020</td>
+          </tr>
+          <tr>
+            <td>Ps5 width playground</td>
+            <td className="withdraw">- R$5.000</td>
+            <td>Playground</td>
+            <td>21/02/2020</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
