64b3d6646f17323be8810230724abf7408edb0c1
ottonsam <ottonsam@gmail.com>
2021-05-27 03:44:36 -0300

    Create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Header } from "./components/Header";
+
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
+    <>
       <GlobalStyle />
-      <h1>dtMoney</h1>
-    </div>
+      <Header />
+    </>
   );
 }
